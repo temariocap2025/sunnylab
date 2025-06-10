f83392e4b35791a002f19d5f6b2dad046da14f0b
AndreAR22 <andrearmasdleon@gmail.com>
2025-06-09 19:10:52 -0600

    base de datos 2 (resumen)

--- sidebar/resumen.php
+++ sidebar/resumen.php
@@ -3,16 +3,56 @@
         <h2 class="current-title">Datos actuales</h2>
     </div>
     
-    <p style="font-size: 1.1rem; line-height: 1.6; color: #374151; margin-bottom: 2rem;">
+    <div class="current-table">
         <?php
             $sql = "SELECT uv_index, ica_index, temperatura, humedad FROM mediciones ORDER BY id_medicion DESC LIMIT 1";
             $result = mysqli_query($connect,$sql);
             $data = mysqli_fetch_assoc($result);
             mysqli_free_result($result);
-            echo "Radiación uv: {$data['uv_index']}, Calidad de aire: {$data['ica_index']}, Temperatura: {$data['temperatura']}, Humedad: {$data['humedad']}";
-            echo "<br> **Work in Progress, esto se cambiará y ajustará posteriormente**";
-            echo "<br> (Datos falsos de fines demostrativos)";
-            mysqli_close($connect);
+
+            $uv_index = $data['uv_index'];
+            $ica_index = $data['ica_index'];
+            $temperatura = $data['temperatura'];
+            $humedad = $data['humedad'];
+
+            function getUVLevel($uv) {
+            if ($uv < 3) return "Bajo ($uv)";
+            elseif ($uv < 6) return "Moderado ($uv)";
+            elseif ($uv < 8) return "Alto ($uv)";
+            elseif ($uv < 11) return "Muy Alto ($uv)";
+            else return "Extremo ($uv)";
+            }
+            function getICALevel($ica) {
+            if ($ica <= 50) return "Buena (ICA $ica)";
+            elseif ($ica <= 100) return "Moderada (ICA $ica)";
+            elseif ($ica <= 150) return "Poco saludable (ICA $ica)";
+            elseif ($ica <= 200) return "Dañina (ICA $ica)";
+            elseif ($ica <= 300) return "Muy Dañina (ICA $ica)";
+            else return "Peligrosa (ICA $ica)";
+            }
             ?>
-    </p>
+        <table>
+            <tr>
+                <th>Radiación UV</th>
+                <th>Calidad de aire</th>
+                <th>Temperatura</th>
+                <th>Humedad</th>
+            </tr>
+            <tr>
+                <td><?php echo getUVLevel($uv_index); ?></td>
+                <td><?php echo getICALevel($ica_index); ?></td>
+                <td><?php echo $temperatura . " °C"; ?></td>
+                <td><?php echo $humedad . " %"; ?></td>
+            </tr>
+        </table>
+    </div>
+    <?php
+    $sql = "SELECT registro_hora FROM mediciones ORDER BY id_medicion DESC LIMIT 1";
+    $result = mysqli_query($connect,$sql);
+    $last_time = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    echo "<br> Útima actualización: " . $last_time["registro_hora"];
+
+    mysqli_close($connect);
+    ?>
 </div>
