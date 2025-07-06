a064b7f7b3d49158bac118798633bdf35b42f413
temariocap2025 <temariocap2025@gmail.com>
2025-07-06 10:50:39 -0600

    avisos bugfixing

--- instagram/check-posts.py
+++ instagram/check-posts.py
@@ -9,9 +9,11 @@ import subprocess
 import os
 import shutil
 import pymysql
+import json
+import shutil
+import tempfile
 
 USERS = ["colegiocapouilliez", "mineducgt"]
-COOKIE = "75508383702:Mso88zNPDrxxZg:4:AYcj-w0JKKa9VgPRecZrWrBQkTESMxwnwt37L-LGIA" #rezar para que no se tenga que refrescar
 
 connection = pymysql.connect(
     host="localhost", port=3306, user="temario", passwd="temario", database="db_solmaforo"
@@ -23,12 +25,138 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 service = Service("/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
+config_path = "/home/temario/.config/gallery-dl/config.json" # gallery-dl
 client = genai.Client(api_key='AIzaSyD4y3882Q0IZjitGCsxxVMafL4RtU8QNeM')
 
+def refresh_cookie(driver):
+    driver.delete_all_cookies()
+    driver.get("https://www.instagram.com/")
+    driver.add_cookie({
+        "name": "sessionid",
+        "value": COOKIE,
+        "domain": ".instagram.com"
+    })
+    print("Cookie refreshed.")
+    driver.get(driver.current_url)
+    time.sleep(5)
+
+def chrome_refresh_cookie():
+    temp_profile_dir = tempfile.mkdtemp()
+    shutil.copytree(
+    '/home/temario/.config/chromium/Default',
+    f'{temp_profile_dir}/Default',
+    ignore=shutil.ignore_patterns('*.lock'),  # ignore lock files (optional)
+    dirs_exist_ok=True,  # if destination exists
+    copy_function=shutil.copy2,
+    ignore_dangling_symlinks=True)
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument(f'--user-data-dir={temp_profile_dir}')  
+    chrome_options.add_argument('--profile-directory=Default')
+    chrome_driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    chrome_driver.get("https://www.instagram.com/")
+    time.sleep(15)
+    cookies = chrome_driver.get_cookies()
+    sessionid_cookie = None
+    for c in cookies:
+        if c['name'] == 'sessionid':
+            sessionid_cookie = c['value']
+            break
+    print(f"SessionID: {sessionid_cookie}")
+
+    update_gallerydl(sessionid_cookie)
+
+    global COOKIE
+    COOKIE = sessionid_cookie
+
+    chrome_driver.quit()
+    shutil.rmtree(temp_profile_dir)
+
+def manual_refresh_cookie():
+    manual_options = Options()
+    manual_options.add_experimental_option("detach", True)
+    manual_driver = webdriver.Chrome(service=service, options=manual_options)
+    manual_driver.get("https://www.instagram.com/")
+    print("Inicia sesión, presiona ENTER cuando ya estes loggeado")
+    input()
+    cookies = manual_driver.get_cookies()
+    sessionid_cookie = None
+    for cookie in cookies:
+        if cookie['name'] == 'sessionid':
+            sessionid_cookie = cookie['value']
+            break
+    manual_driver.quit()
+    if not sessionid_cookie:
+        return
+    print(f"SessionID: {sessionid_cookie}")
+
+    update_gallerydl(sessionid_cookie)
+    global COOKIE
+    COOKIE = sessionid_cookie
+
+def update_gallerydl(new_cookie):
+    try:
+        with open(config_path, "r") as f:
+            config_data = json.load(f)
+
+        config_data["extractor"]["instagram"]["cookies"]["sessionid"] = new_cookie
+
+        with open(config_path, "w") as f:
+            json.dump(config_data, f, indent=4)
+
+    except Exception as e:
+        print("Couldn't update the gallery-dl config file:", e)
+
+def get_pinned_posts(driver):
+    pinned_ids = []
+    try:
+        pinned_icons = driver.find_elements(By.CSS_SELECTOR, 'svg[aria-label="Pinned post icon"]')
+        for icon in pinned_icons:
+            link = icon.find_element(By.XPATH, "./ancestor::a[1]")
+            href = link.get_attribute("href")
+            if "/p/" in href:
+                shortcode = href.split("/p/")[1].split("/")[0]
+                pinned_ids.append(shortcode)
+            elif "/reel/" in href:
+                shortcode = href.split("/reel/")[1].split("/")[0]
+                pinned_ids.append(shortcode)
+    except Exception as e:
+        print("Error getting pinned posts:", e)
+    return pinned_ids
+
+def get_latest_post(driver, pinned_ids):
+    posts = driver.find_elements(By.CLASS_NAME, '_aagu')
+    post_id = 0
+    ext = '.png'
+    for post in posts:
+        try:
+            link = post.find_element(By.XPATH, "./ancestor::a[1]")
+            href = link.get_attribute("href")
+            if "/p/" in href:
+                shortcode = href.split("/p/")[1].split("/")[0]
+                if shortcode not in pinned_ids:
+                    post_id = shortcode
+                    ext = '.png'
+                    break
+            elif "/reel/" in href:
+                shortcode = href.split("/reel/")[1].split("/")[0]
+                if shortcode not in pinned_ids:
+                    post_id = shortcode
+                    ext = '.mp4'
+                    break
+        except Exception as e:
+            print("Error getting post:", e)
+    return post_id, ext
+
+COOKIE = ''
+chrome_refresh_cookie()
 print(f"Monitoring: {USERS}")
 while True:
     for USER in USERS:
         driver.get(f"https://www.instagram.com/{USER}/")
+
         driver.add_cookie({
             "name": "sessionid",
             "value": COOKIE,
@@ -37,42 +165,27 @@ while True:
         driver.get(f"https://www.instagram.com/{USER}/")
         time.sleep(5)
 
-        pinned_icons = driver.find_elements(By.CSS_SELECTOR, 'svg[aria-label="Pinned post icon"]')
-        pinned_post_ids = []
-        for icon in pinned_icons:
-            try:
-                link = icon.find_element(By.XPATH, "./ancestor::a[1]")
-                href = link.get_attribute("href")
-                if "/p/" in href:
-                    shortcode = href.split("/p/")[1].split("/")[0]
-                    pinned_post_ids.append(shortcode)
-                if "/reel/" in href:
-                    shortcode = href.split("/reel/")[1].split("/")[0]
-                    pinned_post_ids.append(shortcode)
-            except Exception as e:
-                print("Error getting pinned post ID:", e)
+        pinned_ids = get_pinned_posts(driver)
+        post_id, ext = get_latest_post(driver, pinned_ids)
 
-        posts = driver.find_elements(By.CLASS_NAME, '_aagu')
-        post_id = 0
-        ext = '.png'
-        for post in posts:
-            try:
-                link = post.find_element(By.XPATH, "./ancestor::a[1]")
-                href = link.get_attribute("href")
-                if "/p/" in href:
-                    shortcode = href.split("/p/")[1].split("/")[0]
-                    if shortcode not in pinned_post_ids:
-                        post_id = shortcode
-                        ext = '.png'
-                        break
-                if "/reel/" in href:
-                    shortcode = href.split("/reel/")[1].split("/")[0]
-                    if shortcode not in pinned_post_ids:
-                        post_id = shortcode
-                        ext = '.mp4'
-                        break
-            except Exception as e:
-                print("Error getting post ID:", e)
+        if post_id == 0:
+            print("Post ID is 0, refreshing cookie...")
+            refresh_cookie(driver)
+
+            pinned_ids = get_pinned_posts(driver)
+            post_id, ext = get_latest_post(driver, pinned_ids)
+
+            if post_id == 0:
+                print("Post ID is still 0, Chrome Cookie refresh:")
+                chrome_refresh_cookie()
+
+                pinned_ids = get_pinned_posts(driver)
+                post_id, ext = get_latest_post(driver, pinned_ids)
+                
+                if post_id == 0:
+                    print("Post ID is still 0, Waiting 30 minutes...")
+                    time.sleep(30 * 60)
+                    continue
 
         with connection.cursor() as cursor:
             sql = "SELECT * FROM instagram WHERE notpost_id = %s"
@@ -126,7 +239,7 @@ while True:
                 except subprocess.CalledProcessError as e:
                     print("Error running gallery-dl:")
                     print(e.stderr)
-                
+
                 image_path = f'gallery-dl/instagram/{USER}/{image_filename}'
                 image_bytes = open(image_path, 'rb').read()
                 image = types.Part.from_bytes(
@@ -166,4 +279,4 @@ while True:
 
             isMedia = None
 
-    time.sleep(5*60)
+    time.sleep(5 * 60)
